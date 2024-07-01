a74f0a31c734825acdb1511eba65510f9f7a532e
nemishv.tagline@.com <nemishv.tagline@gmail.com>
2024-07-01 12:19:51 +0530

    CRUD operation done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
+
   def index
     @articles = Article.all
   end
+
   def show
     @article = Article.find(params[:id])
   end
@@ -9,13 +11,34 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
   def create
-    @article = Article.new(title:"...",body:"...")
+    @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
       render :new,status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :dit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
   end
 
   private
